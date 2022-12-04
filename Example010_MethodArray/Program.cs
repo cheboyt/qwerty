593f5e21337e86451d8dbf1e000ca0f19018a864
cheboyt <cheboyt@gmail.com>
2022-12-04 16:38:45 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿            // 0   1   2  3   4   5   6   7   8
+int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // если найдено сисло  от алгоритм завершил работу успешно
+    }
+    //index = index + 1;
+    index++;
+}
